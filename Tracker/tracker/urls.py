0be39c70faa22d9433de03f38f00821577f1e0d4
jathurchan <admin@jathurchan.com>
2022-10-13 22:17:20 +0200

    Created signin and create new user pages

--- Tracker/tracker/urls.py
+++ Tracker/tracker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signin", views.signin, name="signin"),
+    path("create-user", views.createUser, name="create-user")
 ]
--- Tracker/tracker/views.py
+++ Tracker/tracker/views.py
@@ -5,3 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse("Hello, World!")
+
+def signin(request):
+    return render(request, "tracker/signin.html")
+
+def createUser(request):
+    return render(request, "tracker/create-user.html")
